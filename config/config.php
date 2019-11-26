96644d83710b3374b8994a702dd5cfe434110915
AshanMSilva <ashan.silva.97.aj@gmail.com>
2019-11-26 13:15:50 +0530

    change f to P in config

--- config/config.php
+++ config/config.php
@@ -2,5 +2,5 @@
     define('DEBUG',true);
     define('DEFAULT_LAYOUT','default'); //if there is no layout controller use this layout
     define('DEFAULT_CONTROLLER','Home'); //default controller if there isn't one defined in the url
-    define('SITE_TITLE','E-Commerce-FlatForm'); //if there is no site title this will be the the site title
+    define('SITE_TITLE','E-Commerce-PlatForm'); //if there is no site title this will be the the site title
     define('PROOT','/Single-Vendor-E-Commerce-Platform/'); //set this to '/' for a live server
