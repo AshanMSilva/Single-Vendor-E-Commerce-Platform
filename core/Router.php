e140760903d2b07c1b2001cb49d4e5d11cd6de99
AshanMSilva <ashan.silva.97.aj@gmail.com>
2019-12-06 16:37:10 +0530

    controller, router class updated

--- core/Router.php
+++ core/Router.php
@@ -1,28 +1,37 @@
 <?php
-    class Router{
-        public static function route($url){
-         
-            
-            $controller = (isset($url[0]) && $url[0] !='') ? ucwords($url[0]) : DEFAULT_CONTROLLER; 
-            $controller_name = $controller;
-            array_shift($url);
 
-            //action
-            $action = (isset($url[0]) && $url[0] !='') ? $url[0].'Action' : indexAction; 
-            $action_name = $controller;
-            array_shift($url);
-        
-            //params
-            $queryParams =$url;
+class Router {
 
-            $dispatch = new $controller($controller_name,$$action);   ///class name
-        
-            if(method_exists($controller,$action)){
-                call_user_func_array([$dispatch,$action],$queryParams);
-            }
-            else{
-                die("that method doesn't exist in the controller ".$controller_name);
-            }
-        }
+	public static function route($url){
+		//dnd($url);		// helper function defned in app/lib/helpers/helpers.php
+		
+		//controller
+		$controller = (isset($url[0]) && $url[0] != '') ? ucwords($url[0]) : DEFAULT_CONTROLLER;
+		$controller_name = $controller;
+		array_shift($url);
+		
+		//dnd($url);
+		//dnd($controller);
 
-    }
+		$action = (isset($url[0]) && $url[0] != '') ? $url[0] . 'Action' : 'indexAction';
+		$action_name = $controller;
+		array_shift($url);
+		//echo $controller . '<br>';
+		//echo $action . '<br>';
+		//echo $action_name;
+		//dnd($url);
+
+		//parameters
+		$queryParams = $url;	//not handled parameters with spaces 
+
+		$dispatch = new $controller($controller_name, $action);
+		//dnd($dispatch);
+
+		if (method_exists($controller, $action)){
+			call_user_func_array([$dispatch, $action], $queryParams);	// $dispatch->registerAction($queryParams);
+		}
+		else{
+			die("That method does not exist in the controller - " . $controller_name);
+		}
+	}
+}
