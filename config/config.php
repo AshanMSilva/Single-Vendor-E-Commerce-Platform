fc4a4590b01880bbacee79dedd1e75da23a7ddcf
AshanMSilva <ashan.silva.97.aj@gmail.com>
2019-12-06 16:03:29 +0530

    chnged config

--- config/config.php
+++ config/config.php
@@ -3,13 +3,13 @@
 define('DEBUG', true);
 
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'ruah');
+define('DB_NAME', 'e_commerce_platform');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
 define('DEFAULT_CONTROLLER', 'Home');	// default controller if there isn't one defined in the url
 define('DEFAULT_LAYOUT', 'default');	// if no layout is set in the controller, use this layout
 
-define('PROOT', '/ruah/');		// set this to '/' for a live server
+define('PROOT', '/Single-Vendor-E-Commerce-Platform/');		// set this to '/' for a live server
 
-define('SITE_TITLE', 'Ruah MVC Framework');		// this wiil be used if no site title is set
+define('SITE_TITLE', 'Single Vendor E Commerce Platform');		// this wiil be used if no site title is set
