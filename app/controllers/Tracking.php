50edc6bd0231a01accfc6ed76d6ae84ff96203c5
AshanMSilva <ashan.silva.97.aj@gmail.com>
2019-12-25 09:25:26 +0530

    bugs

---
+++ app/controllers/Tracking.php
@@ -0,0 +1,8 @@
+<?php
+    class Tracking extends Controller{
+        public function __construct($controller,$action){
+            parent::__construct($controller,$action);
+        }
+
+        
+    }
