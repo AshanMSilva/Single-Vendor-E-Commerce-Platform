fc8a6db55aff4e4789adf46a05f16ca80677d9af
KaveeshaSIlva <47806601+KaveeshaSIlva@users.noreply.github.com>
2019-12-26 21:58:43 +0530

    methods changed

--- app/controllers/Product.php
+++ app/controllers/Product.php
@@ -8,47 +8,19 @@
             $title=$_GET["title"];
             Session::set('title',$title);
             Session::set('product_id',$product_id);
-            $con=DB::getInstance();
-            $attributes=$this->getAttributes($product_id,$con);
+            
+            $attributes=Product::get_attributes($product_id);
             Session::set('attributes',$attributes);
-
-            //get the category
-            $sql="SElECT title FROM categories join product_category_relations using (category_id) where product_id = ?";
-            $category_name=$con->query($sql,[$product_id])->results()[0]->title;
+            
+            $details=Product::get_details_of_product($product_id);
+            $category_name=$details["category"];
+            $brand=$details["brand"];
             Session::set('category_name',$category_name);
+            Session::set('brand_name',$brand);
 
-            //get the brand
-            $brand_name=$con->query("SELECT brand from products where product_id= ?",[$product_id])->results()[0]->brand;
-            Session::set('brand_name',$brand_name);
             $this->view->setLayout('normal');
-            $this->view->render('product/productDisplay');
+            $this->view->render('singleproduct/productDisplay');
         }
-
-        //this function returns the all attributes for given product
-        function getAttributes($product_id,$con){
-            $attributes=array();
-            $sql="select attribute_name,value from attributes  where product_id= ?"; 
-            $result = $con->query($sql,[$product_id]);
-
-
-            $rowcount=$result->get_row_count();
-            for($i=0;$i<$rowcount;$i++){
-                $attribute_name=$result->results()[$i]->attribute_name;
-                $value=$result->results()[$i]->value;
-                
-                if(array_key_exists($attribute_name, $attributes)==false){
-                    $attributes[$attribute_name]=array();
-                    array_push($attributes[$attribute_name],$value);
-                }
-                else{
-                    if(in_array($value,$attributes[$attribute_name])==false){
-                        array_push($attributes[$attribute_name],$value);
-                    }
-                }
-            }
-            return $attributes;            
-        }
-
         public function productAddAction(){
 
 
--- app/controllers/Search.php
+++ app/controllers/Search.php
@@ -5,87 +5,56 @@
         }
 
         public function searchResultAction(){
-            $con=DB::getInstance();
-
             if(isset($_POST["key"])){
                 $x= Input::get('key');
             }
+<<<<<<< Updated upstream
+=======
+            Session::set('xx',$x);
+            $x=Session::get('xx');
+>>>>>>> Stashed changes
             //call function
-            $lastView=$this->searchItem($x,$con);
+            $lastView=$this->searchItem($x);
             $prodDetails=array();
             foreach($lastView as $product_id){
-                $prodDetails[$product_id]=$this->getDetailsOfProduct($product_id,$con);
+                $prodDetails[$product_id]=$this->getDetailsOfProduct($product_id);
             }
             Session::set('x',$x);
             Session::set('prodDetails',$prodDetails);
+<<<<<<< Updated upstream
 
             $this->view->setLayout('default');
+=======
+            $this->view->setLayout('normal');
+>>>>>>> Stashed changes
             $this->view->render('search/searchResult');
         }
         
         //this function returns the all products in the given category
         //this function is called in searchResultAction
-        public function viewProductsInCategory($category_id,$con,$arr){ 
-
-            // //database connection
-            $con=DB::getInstance();
-            $sqlstmt="SELECT value  FROM attributes WHERE attribute_name= ?";
-            $res=$con->query($sqlstmt,["colour"]);
-            $c=$res->get_row_count();
-            for($i=0;$i<$c;$i++){
-                $f=$res->results()[$i]->value;
-            }
-            //check for sub categories    
-            $sqlSubCat="SELECT sub_category_id  FROM category_relations WHERE category_id= ? ";
-            $resultSubCat=$con->query($sqlSubCat,[$category_id]);
-            $len=$resultSubCat->get_row_count();                                   
-            if ($len=="0"){ // no sub category - simple category  
-                // just display the products of this category
-                $sqlProducts="SELECT product_id  FROM  product_category_relations join products using (product_id) WHERE category_id= ? ";
-                $resultProducts=$con->query($sqlProducts,[$category_id]);
-                $rowcount=$resultProducts->get_row_count();
-                for($i=0;$i<$rowcount;$i++){
-                    $product_id=$resultProducts->results()[$i]->product_id;
-                    if(in_array($product_id,$arr)==false){
-                        array_push($arr,$product_id);                 
-                    }               
-                }return $arr;        
-            }
-            else{ // if this category is a complex category
-                for($i=0;$i<$len;$i++){
-                    $sub_category_id=$resultSubCat->results()[$i]->sub_category_id; 
-                    return array_merge($arr,$this->viewProductsInCategory($sub_category_id,$con,$arr)); // recursion 
-                }
-            }
+        public function viewProductsInCategory($category_id,$arr){ 
+            return Category::get_products_in_category($category_id);
         } 
+
+
         //this function returns all matching products for the given key
         //this function is called in searchResultAction
-        public function searchItem($key,$con){
+        public function searchItem($key){
             $lastView=array();
-            //if key is a category name
             $x="%".$key."%";
-            $sqlCat="SELECT category_id FROM categories WHERE title like ? ";
-            $resultCat=$con->query($sqlCat,[$x]);
-
-            //view products in categories
-            $rowcount=$resultCat->get_row_count();
-            for($i=0;$i<$rowcount;$i++){
-                $category_id=$resultCat->results()[$i]->category_id;
-                $lastView=array_merge($lastView,$this->viewProductsInCategory($category_id,$con,$lastView));               
+            $categories=Category::get_categories_from_key($x);
+            foreach($categories as $category_id){
+                $lastView=array_merge($lastView,$this->viewProductsInCategory($category_id,$lastView));               
             }                                                             
-            // if key is a product name
-            $sql="SELECT product_id  FROM products WHERE title like ? ";
-            $result=$con->query($sql,[$x]);
-
-            //view products
-            $rowcount=$result->get_row_count();
-            for($i=0;$i<$rowcount;$i++){
-                $product_id=$result->results()[$i]->product_id;
+            
+            // if key is a product name 
+            // view products
+            $products=Product::get_products_from_key($x);
+            foreach($products as $product_id){
                 if(in_array($product_id,$lastView)==false){
                     array_push($lastView,$product_id);
-                }
+                } 
             }
-
             //get distinct product_ids
             $distinct=array();
             foreach($lastView as $product_id){
@@ -96,18 +65,7 @@
 
             return $distinct;
         }
-        public function getDetailsOfProduct($product_id,$con){
-            
-            // sql query to get the title
-            $sqlTitleResult="SELECT title FROM products WHERE product_id= ? ";
-            $titleResult=$con->query($sqlTitleResult,[$product_id]);
-
-            $title=$titleResult->results()[0]->title;
-            //query to get the price range
-            $sqlPrices="SELECT MIN(price) as minPrice,MAX(price) as maxPrice  from variants where product_id= ? ";
-            $PriceResult=$con->query($sqlPrices,[$product_id]);
-            $minPrice=$PriceResult->results()[0]->minPrice;
-            $maxPrice=$PriceResult->results()[0]->maxPrice;
-            return array($title,$minPrice,$maxPrice);
+        public function getDetailsOfProduct($product_id){
+            return Product::get_details_of_product($product_id);
         }
 }
