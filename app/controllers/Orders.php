a573fc74448e59208a47b65390aef55c69ceae23
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2019-12-28 23:23:28 +0530

    order view front end 70%

--- app/controllers/Orders.php
+++ app/controllers/Orders.php
@@ -51,7 +51,65 @@ class Orders extends Controller{
 						
 	}
 
-	public static function processTable(){
+	public static function processTable($customer_id){
 		//if zero display no order else display orders in table
+		$orders = [1,3,2,4];
+		if (count($orders)==0) {
+			echo "   <br> <br><br> 
+			<section class='tracking_box_area section_gap'>
+				<div class = 'whole-wrap pb-100'>
+					<div class = 'container'><h3 class='mb-30'>No previous orders to display</h3></div>
+				</div>
+			</section>";
+		} else {
+			self::displayTrackbox();
+			echo "    <!--================Order_details Area =================-->
+				<div class='whole-wrap pb-100'>
+						<div class='container'>
+				    			<div class='section-top-border'>
+								<h3 class='mb-30'>Previous orders</h3>
+								<div class='progress-table-wrap'>
+									<div class='progress-table'>
+										<div class='table-head'>
+											<div class='serial'>Order ID</div>
+											<div class='country'>Date</div>
+											<div class='visit'>Total Price</div>
+											<div class='visit'>Current Location</div>
+											<div class='visit'>	</div>
+										</div>";
+			foreach ($orders as $value) {
+				Orders::displayRow($value);
+			}
+					echo "</div>
+						</div>
+					</div>
+			</div>
+		</div>    
+		<!--================End Order details Area =================-->";
+		}
+		
 	} 
+
+	public static function displayTrackbox(){
+		$action = '<?=PROOT?>tracking/trackorderu';
+		echo "    <!--================Tracking Box Area =================-->
+    <section class='tracking_box_area section_gap'>
+        <div class='container'>
+            <div class='tracking_box_inner'>
+            	<br>
+            	<br>
+                <p>To track your order please enter your Order ID in the box below and press the 'Track' button.</p>
+                <form class='row tracking_form' action='<?=PROOT?>tracking/trackorderu' method='post' novalidate='novalidate'>
+                    <div class='col-md-12 form-group'>
+                        <input type='text' class='form-control' id='order' name='order' placeholder='Order ID' onfocus='this.placeholder = ''' onblur='this.placeholder = 'Order ID''>
+                    </div>
+                    <div class='col-md-12 form-group'>
+                        <button type='submit' value='submit' name='submit' class='primary-btn'>Track Order</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+        <!--================End Tracking Box Area =================-->";
+	}
 }
